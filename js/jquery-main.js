78f7f5ce69c2d6e2cdef5fcd18524d88a68042e4
zain-jr <zainjrtech21@gmail.com>
2016-11-24 18:30:13 +0500

    acha

--- js/jquery-main.js
+++ js/jquery-main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	if($(".js-example-basic-single").length)
-		$(".js-example-basic-single").select2();
+		$(".js-example-basic-single").select2();	
 	if (screen.width < 768){
 		$('.call-agent-btn').each(function(){
 			var mobileNumber = $(this).attr('data-tel');
